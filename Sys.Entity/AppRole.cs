53c21c910e95751315d7c8a95f8c20c0e1ab2ffb
Huy <huysieu@gmail.com>
2017-11-11 09:02:36 +0700

    Revert "20171111"
    This reverts commit d69f712cbaa5b1498a5f149ca7fa513b3ef16ebf.

--- Sys.Entity/AppRole.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
-
-namespace Sys.Entity
-{
-    public class AppRole : IdentityRole
-    {
-        public string Description { get; set; }
-    }
-}
